1b17ca77294d195d2d86dddd9cc7b65cbfcd06ce
Vincent Hanquez <vincent@snarc.org>
2014-01-05 16:12:43 +0000

    initial commit

---
+++ TVM/Qemu.hs
@@ -0,0 +1,176 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE OverloadedStrings #-}
+-- |
+-- Module      : TVM.Qemu
+-- License     : BSD-style
+-- Maintainer  : Vincent Hanquez <vincent@snarc.org>
+-- Stability   : experimental
+-- Portability : Linux
+--
+-- Qemu CLI wrapper for TVM
+--
+module TVM.Qemu where
+
+import Data.Data
+import Data.Maybe
+import Data.List
+import Data.Aeson ()
+import Data.Aeson.Generic
+import qualified Data.ByteString.Lazy as B
+import System.FilePath
+import System.Directory
+import Control.Monad
+import Control.Applicative
+
+import System.Process
+
+data BootOrder = BootDisk | BootCdrom
+    deriving (Show,Read,Eq,Typeable,Data)
+data DiskInterface = DiskSCSI | DiskIDE | DiskVirtIO
+    deriving (Show,Read,Eq,Typeable,Data)
+data DiskMedia = MediaDisk | MediaCDROM
+    deriving (Show,Read,Eq,Typeable,Data)
+
+data Disk = Disk
+    { diskFile      :: String
+    , diskInterface :: Maybe DiskInterface
+    , diskMedia     :: DiskMedia
+    } deriving (Show,Read,Eq,Typeable,Data)
+
+data Nic = Nic
+    { nicModel   :: Maybe String
+    , nicMacAddr :: Maybe String -- FIXME refine type
+    , nicVLAN    :: Maybe Int
+    } deriving (Show,Read,Eq,Typeable,Data)
+
+defaultNic :: Nic
+defaultNic = Nic Nothing Nothing Nothing
+
+data NetFamily = TCP | UDP
+    deriving (Show,Read,Eq,Typeable,Data)
+
+-- hostfwd=[tcp|udp]:[hostaddr]:hostport-[guestaddr]:guestport
+data HostFWD = HostFWD
+    { hostFWDFamily :: Maybe NetFamily
+    , hostFWDHostAddr :: Maybe String
+    , hostFWDHostPort :: Int
+    , hostFWDGuestAddr :: Maybe String
+    , hostFWDGuestPort :: Int
+    } deriving (Show,Read,Eq,Typeable,Data)
+
+data Net = NetUser
+    { userNetHostFWD :: [HostFWD]
+    } deriving (Show,Read,Eq,Typeable,Data)
+
+data Serial = SerialPipe String
+    deriving (Show,Read,Eq,Typeable,Data)
+
+data VGA = VGA_Standard | VGA_Cirrus | VGA_QXL | VGA_VMWare | VGA_None
+    deriving (Show,Read,Eq,Typeable,Data)
+
+data QemuArch = ArchX86_64 | ArchI386
+    deriving (Show,Read,Eq,Typeable,Data)
+
+data Qemu = Qemu
+    { qemuArch    :: QemuArch
+    , qemuMemory  :: Int -- in megabytes
+    , qemuCPUs    :: Int
+    , qemuBoot    :: BootOrder
+    , qemuDrives  :: [Disk]
+    , qemuNics    :: [Nic]
+    , qemuNets    :: [Net]
+    , qemuSerials :: [Serial]
+    , qemuVGA     :: VGA
+    , qemuVNC     :: Maybe Int
+    } deriving (Show,Read,Eq,Typeable,Data)
+
+defaultQemu = Qemu
+    { qemuArch    = ArchX86_64
+    , qemuMemory  = 1024
+    , qemuCPUs    = 1
+    , qemuBoot    = BootDisk
+    , qemuDrives  = []
+    , qemuNics    = []
+    , qemuNets    = []
+    , qemuSerials = []
+    , qemuVGA     = VGA_Standard
+    , qemuVNC     = Nothing
+    }
+
+class ShowArg a where
+    toCLIArg :: a -> String
+
+instance ShowArg QemuArch where
+    toCLIArg ArchX86_64 = "x86_64"
+    toCLIArg ArchI386   = "i386"
+
+instance ShowArg BootOrder where
+    toCLIArg BootDisk = "c"
+    toCLIArg BootCdrom = "d"
+
+instance ShowArg DiskInterface where
+    toCLIArg DiskSCSI   = "scsi"
+    toCLIArg DiskIDE    = "ide"
+    toCLIArg DiskVirtIO = "virtio"
+
+instance ShowArg DiskMedia where
+    toCLIArg MediaDisk  = "disk"
+    toCLIArg MediaCDROM = "cdrom"
+
+instance ShowArg VGA where
+    toCLIArg VGA_Standard = "std"
+    toCLIArg VGA_Cirrus   = "cirrus"
+    toCLIArg VGA_QXL      = "qxl"
+    toCLIArg VGA_VMWare   = "vmware"
+    toCLIArg VGA_None     = "none"
+
+instance ShowArg NetFamily where
+    toCLIArg TCP = "tcp"
+    toCLIArg UDP = "udp"
+
+instance ShowArg HostFWD where
+    -- hostfwd=[tcp|udp]:[hostaddr]:hostport-[guestaddr]:guestport
+    toCLIArg hf = intercalate "-" $ map (intercalate ":")
+                    [ [ maybe "" toCLIArg (hostFWDFamily hf)
+                      , maybe "" id (hostFWDHostAddr hf)
+                      , show (hostFWDHostPort hf)
+                      ]
+                    , [ maybe "" id (hostFWDGuestAddr hf)
+                      , show (hostFWDGuestPort hf)
+                      ]
+                    ]
+
+toCLI :: FilePath -> FilePath -> Qemu -> [String]
+toCLI serialDir diskDir qemu =
+    ["qemu-system-" ++ toCLIArg (qemuArch qemu)] ++
+    ["-enable-kvm"] ++
+    ["-m", show (qemuMemory qemu)] ++
+    ["-smp", show (qemuCPUs qemu)] ++
+    ["-boot", toCLIArg (qemuBoot qemu)] ++
+    concatMap cliDisk (qemuDrives qemu) ++
+    concatMap cliNic (qemuNics qemu) ++
+    concatMap cliNet (qemuNets qemu) ++
+    concatMap cliSerial (qemuSerials qemu)
+  where cliDisk disk     = [ "-drive", kvs [ Just ("file", intercalate ",," $ wordsWhen (== ',') (resolveDir diskDir $ diskFile disk))
+                                           , (\v -> ("if", toCLIArg v)) `fmap` diskInterface disk
+                                           , Just ("media", toCLIArg $ diskMedia disk)
+                                           ] ]
+        cliNet net       = [ "-net", "user" `comma` (kvs $ map (\v -> Just ("hostfwd", toCLIArg v)) (userNetHostFWD net))]
+        cliNic nic       = [ "-net", "nic" `comma` kvs [ (\v -> ("model", v)) `fmap` nicModel nic
+                                                       , (\v -> ("vlan", show v)) `fmap` nicVLAN nic
+                                                       , (\v -> ("macaddr", v)) `fmap` nicMacAddr nic
+                                                       ] ]
+        cliSerial (SerialPipe p) = [ "-serial", "pipe:" ++ (resolveDir serialDir p) ]
+        kvs l = intercalate "," $ map (\(k,v) -> k ++ "=" ++ v) $ catMaybes l
+
+        comma s v | null v    = s
+                  | otherwise = s ++ "," ++ v
+
+        wordsWhen     :: (Char -> Bool) -> String -> [String]
+        wordsWhen p s =  case dropWhile p s of
+                              "" -> []
+                              s' -> w : wordsWhen p s''
+                                    where (w, s'') = break p s'
+
+resolveDir rDir path | isAbsolute path = path
+                     | otherwise       = rDir </> path
---
+++ tvm.hs
@@ -0,0 +1,226 @@
+--
+-- Copyright (C) 2013 Vincent Hanquez <vincent@snarc.org>
+-- License: BSD3
+--
+-- Tiny VM Manager
+--
+module Main where
+
+import TVM.Qemu
+import Data.Data
+import Data.Maybe
+import Data.List
+import Data.Aeson ()
+import Data.Aeson.Generic
+import qualified Data.ByteString.Lazy as B
+import System.Environment
+import System.Exit
+import System.Console.GetOpt
+import System.FilePath
+import System.Directory
+import Control.Monad
+import Control.Applicative
+import Text.Groom
+
+import System.IO
+import System.Process
+import System.Posix.Process (forkProcess, executeFile, getProcessStatus, getProcessID)
+import System.Posix.Files (createNamedPipe, getFileStatus, isNamedPipe)
+
+type Name = String
+
+pidPath tvm name = runningDir tvm </> (name ++ ".pid")
+cfgPath tvm name = configDir tvm </> (name ++ ".cfg")
+
+readCfg :: TVMConfig -> Name -> IO (Maybe Qemu)
+readCfg tvm name = do
+    decode <$> B.readFile (cfgPath tvm name)
+withCfg tvm name f = readCfg tvm name >>= maybe (return $ error ("cannot open config : " ++ name)) f
+writeCfg tvm name config = B.writeFile (cfgPath tvm name) (encode config)
+
+listCfg tvm = do
+    files <- filter (isSuffixOf ".cfg") <$> getDirectoryContents (configDir tvm)
+    return $ sort $ map (reverse . drop 4 . reverse) files
+
+readPid :: TVMConfig -> String -> IO Int
+readPid tvm name =
+   read <$> readFile (pidPath tvm name)
+
+deletePid tvm name = removeFile (pidPath tvm name)
+
+getVMPid :: TVMConfig -> String -> IO (Maybe Int)
+getVMPid tvm name = do
+    pidFileExist <- doesFileExist (pidPath tvm name)
+    if pidFileExist
+        then do pid <- read <$> readFile (pidPath tvm name)
+                dirExist <- doesDirectoryExist ("/proc/" ++ show pid)
+                if dirExist
+                    then do
+                        cmdline <- readFile ("/proc/" ++ show pid ++ "/cmdline")
+                        if "qemu-" `isPrefixOf` cmdline
+                            then return $ Just pid 
+                            else return Nothing
+                    else return Nothing
+        else return Nothing
+
+withRunningVM tvm name f = do
+    mpid <- getVMPid tvm name
+    case mpid of
+        Nothing  -> putStrLn (name ++ " is not running")
+        Just pid -> f pid
+
+isRunning tvm name = maybe False (const True) <$> getVMPid tvm name
+ 
+data TVMConfig = TVMConfig
+    { rootDir    :: FilePath
+    , diskDir    :: FilePath
+    , configDir  :: FilePath
+    , consoleDir :: FilePath
+    , runningDir :: FilePath
+    }
+
+doCreate tvm args =
+    case args of
+        name:[] -> do
+            names <- listCfg tvm
+            when (name `elem` names) $ error ("config " ++ name ++ " already exist")
+            let cfg = defaultQemu
+                    { qemuDrives = [ Disk { diskFile = diskDir tvm </> (name ++ ".dsk"), diskInterface = Just DiskVirtIO, diskMedia = MediaDisk }
+                                   , Disk { diskFile = "isoFile.iso", diskInterface = Nothing, diskMedia = MediaCDROM }
+                                   ]
+                    , qemuNics = [ defaultNic { nicModel = Just "e1000" } ]
+                    , qemuNets = [ NetUser { userNetHostFWD = [ HostFWD Nothing Nothing 4022 Nothing 22 ] } ]
+                    , qemuSerials = [ SerialPipe name ]
+                    }
+            writeCfg tvm name cfg
+        _       -> error "usage: tvm create <name>"
+
+
+doList tvm args = do
+    names <- listCfg tvm
+    forM_ names $ \name -> do
+        pid <- getVMPid tvm name
+        putStrLn (name ++ (maybe " [stopped]" (\p -> "[running pid=" ++ show p ++ "]") pid))
+
+data StartOpts = StartVNC | StartConsole
+    deriving (Show,Eq)
+
+doStart tvm args =
+    case getOpt Permute options args of
+        (o,n,[]  ) -> start o n
+        (_,_,errs) -> ioError (userError (concat errs ++ usageInfo "usage: tvm start [--vnc] <name>" options))
+  where options =
+            [ Option ['v'] ["vnc"] (NoArg StartVNC) "start a vnc viewer"
+            , Option ['c'] ["console"] (NoArg StartConsole) "start the console"
+            ]
+        -- actual start
+        start opts [name] = withCfg tvm name $ \cfg -> do
+            let cmds = toCLI (consoleDir tvm) (diskDir tvm) cfg
+            let pidFile = pidPath tvm name
+            -- it's of course racy..
+            mpid <- getVMPid tvm name
+            when (isJust mpid) $ error (name ++ " is already running")
+
+            prepareSerials cfg
+
+            pid <- forkProcess $ startProcess pidFile cmds
+            _   <- getProcessStatus False False pid
+
+            -- FIXME start a vnc viewer
+            when (StartVNC `elem` opts) $ return ()
+            when (StartConsole `elem` opts) $ doConsole tvm [name]
+            exitSuccess
+        start _ _ = error "usage: tvm start <name>"
+
+        startProcess pidFile (cmd:args) = do
+            pid <- forkProcess $ exec pidFile cmd args
+            _   <- getProcessStatus False False pid
+            exitSuccess
+        exec pidFile cmd args = do
+            myPid <- getProcessID
+            writeFile pidFile (show myPid)
+            putStrLn ("starting qemu as pid: " ++ show myPid)
+            executeFile cmd True args Nothing
+
+        -- check if all serials file are available, and if not create them
+        prepareSerials cfg = mapM_ checkFifos $ concatMap prep $ qemuSerials cfg
+          where prep (SerialPipe path) =
+                    let p = resolveDir (consoleDir tvm) path
+                     in [p ++ ".in", p ++ ".out"]
+                checkFifos path = do
+                    exists <- doesFileExist path
+                    if exists
+                        then do
+                            fstat <- getFileStatus path
+                            if isNamedPipe fstat
+                                then return ()
+                                else error ("serial " ++ path ++ " already exist but is not a fifo")
+                        else createNamedPipe path 0o0700
+
+doStop tvm args = do
+    case args of
+        [name] -> withRunningVM tvm name $ \pid -> do
+                     deletePid tvm name
+                     ec <- rawSystem "kill" [ show pid ]
+                     exitWith ec
+        _ -> error "usage: tvm stop <name>"
+
+doAdd tvm name args = undefined
+doSet tvm name args = undefined
+doGet tvm name args = undefined
+
+doInfo tvm name args = withCfg tvm name $ \cfg -> do
+    field "arch  " (qemuArch cfg)
+    field "memory" (qemuMemory cfg)
+    field "cpus  " (qemuCPUs cfg)
+    field "boot  " (qemuBoot cfg)
+    field "vga   " (qemuVGA cfg)
+    list "drives" (qemuDrives cfg) $ \disk ->
+        putStrLn ("file=" ++ diskFile disk ++ " if=" ++ show (diskInterface disk) ++ " media=" ++ show (diskMedia disk))
+    list "nics" (qemuNics cfg) (\nic -> putStrLn $ groom nic)
+    list "nets" (qemuNets cfg) (\net -> putStrLn $ groom net)
+    list "serials" (qemuSerials cfg) (\net -> putStrLn $ groom net)
+  where field k v = putStrLn (k ++ ": " ++ show v)
+        list name l printer = putStrLn name >> mapM_ (\(i,f) -> putStr ("  [" ++ show i ++ "] ") >> printer f) (zip [0..] l)
+
+doConsole tvm args =
+    case args of
+        [name] -> withRunningVM tvm name $ \_ ->
+                  withCfg tvm name $ \cfg -> do
+                      let path = case qemuSerials cfg of
+                                    SerialPipe p:_ -> resolveDir (consoleDir tvm) p ++ ".out"
+                                    []             -> error "no serial defined"
+                      withFile path ReadMode $ \h ->
+                          B.hGetContents h >>= B.putStr 
+                      return ()
+        _ -> error "usage: tvm stop <name>"
+
+main = do
+    home <- getHomeDirectory
+    let tvmDir = home </> ".tvm"
+        tvm = TVMConfig { rootDir    = tvmDir
+                        , configDir  = tvmDir </> "config"
+                        , diskDir    = tvmDir </> "disk"
+                        , consoleDir = tvmDir </> "console"
+                        , runningDir = tvmDir </> "running"
+                        }
+    dirExist <- doesDirectoryExist (rootDir tvm)
+    unless dirExist $ do
+        mapM_ createDirectory $ map (flip ($) tvm) [rootDir, configDir, diskDir, consoleDir, runningDir]
+    args <- getArgs
+    case args of
+        "create":xs -> doCreate tvm xs
+        "list":xs   -> doList tvm xs
+        "start":xs  -> doStart tvm xs
+        "stop":xs   -> doStop tvm xs
+        "info":name:xs -> doInfo tvm name xs
+        "add":name:xs -> doSet tvm name xs
+        "set":name:xs -> doSet tvm name xs
+        "get":name:xs -> doGet tvm name xs
+        "console":xs -> doConsole tvm xs
+        "info":[]  -> error "need a name"
+        "add":[]   -> error "need a name"
+        "set":[]   -> error "need a name"
+        "get":[]   -> error "need a name"
+        _:_        -> error "unknown command"
+        []         -> error "no argument"
