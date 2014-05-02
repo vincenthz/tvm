ac5f8ba5019396e7d483d2b18cd27aa0a74ffb68
Vincent Hanquez <vincent@snarc.org>
2014-05-02 09:59:22 +0100

    add support for backing file

--- TVM/Disk.hs
+++ TVM/Disk.hs
@@ -8,17 +8,17 @@ import System.Exit
 data DiskType = Qcow2 | Qcow | Raw
     deriving (Show,Eq)
 
-createDisk :: FilePath -- ^ disk path
+createDisk :: Maybe FilePath -- ^ optional backing file
+           -> FilePath -- ^ disk path
            -> Int      -- ^ size in megabytes
            -> DiskType -- ^ disk type
            -> IO ()
-createDisk filepath sizeMb diskType = do
-    ec <- rawSystem "qemu-img" cli
+createDisk backingFile filepath sizeMb diskType = do
+    ec <- rawSystem "qemu-img" ("create" : cli)
     when (ec /= ExitSuccess) $ do
         putStrLn (error "creating disk: " ++ show ec)
         exitFailure -- shouldn't exit, but just return error
-  where cli = [ "create"
-              , "-f", map toLower (show diskType)
-              , filepath
-              , show sizeMb ++ "M"
-              ]
+  where cli =
+            maybe [] (\b -> ["-b", b]) backingFile ++
+            [ "-f", map toLower (show diskType), filepath ] ++
+            maybe [] (const [show sizeMb ++ "M"]) backingFile
